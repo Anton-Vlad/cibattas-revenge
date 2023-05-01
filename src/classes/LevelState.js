6ac252e881cb9f924bf904567ae758553ddcceb8
Anton-Vlad <anton.vlad_viorel@yahoo.com>
2023-05-01 16:21:58 +0300

    9. The Game Loop

--- src/classes/LevelState.js
+++ src/classes/LevelState.js
@@ -1,4 +1,5 @@
 import { LEVEL_THEMES, PLACEMENT_TYPE_HERO, PLACEMENT_TYPE_GOAL } from "../helpers/consts";
+import { GameLoop } from "./GameLoop";
 import { placementFactory } from "./PlacementFactory";
 
 export class LevelState {
@@ -19,7 +20,26 @@ export class LevelState {
             { id: 1, x: 6, y: 4, type: PLACEMENT_TYPE_GOAL },
         ].map(config => {
             return placementFactory.createPlacement(config, this)
+        });
+
+        this.startGameLoop();
+    }
+
+    startGameLoop() {
+        this.gameLoop?.stop();
+        this.gameLoop = new GameLoop(() => {
+            this.tick();
         })
+    }
+
+    tick() {
+        // Call tick()  on every placement that wants to Update
+        this.placements.forEach((placement) => {
+            placement.tick();
+        });
+
+        // Emit any changes that occour to React
+        this.onEmit(this.getState());
     }
 
     getState() {
--- src/game-objects/HeroPlacement.js
+++ src/game-objects/HeroPlacement.js
@@ -3,6 +3,10 @@ import Sprite from "../components/object-graphics/Sprite";
 import { TILES } from "../helpers/tiles";
 
 export class HeroPlacement extends Placement {
+    tick() {
+        console.log('Update the HERO')
+    }
+
     renderComponent() {
         return <Sprite frameCoord={TILES.HERO_RIGHT} size={32} />
     }
